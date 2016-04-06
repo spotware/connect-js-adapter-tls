63da2ad5a29c45b87dfdc95924e897ecb3182fe1
vspirin <vspirin@spotware.com>
2016-04-06 17:08:11 +0300

    add tests

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var AdapterTLS = require('../index');
+
+describe('AdapterTLS', function () {
+
+    xit('constructor', function () {});
+    xit('connect', function () {});
+    xit('send', function () {});
+});
