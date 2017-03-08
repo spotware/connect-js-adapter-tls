e2933c8582d75bae89a3c39fd3dc5c55c71492d9
vspirin <vspirin@spotware.com>
2017-03-08 17:43:06 +0200

    support multi-subscribe with help state-emitter

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 'use strict';
 
 var tls = require('tls');
+var StateEmitter = require('state-emitter').StateEmitter;
 
 var AdapterTLS = function (params) {
     this.host = params.host;
     this.port = params.port;
-    this.socket = undefined;
+    this.state = new StateEmitter(undefined);
 };
 
 AdapterTLS.prototype.connect = function () {
-    var socket = tls.connect(this.port, this.host, this.onOpen);
+    var
+        state = this.state,
+        socket = tls.connect(this.port, this.host, function () {
+            state.next(true);
+        });
 
-    socket.on('data', this.onData);
-    socket.on('end', this.onEnd);
-    socket.on('error', this.onError);
-
-    this.socket = socket;
+    socket.on('data', this.ondata);
+    socket.on('end', function () {
+        state.next(false);
+    });
+    socket.on('error', function () {
+        state.next(false);
+    });
+    this.send = function (data) {
+        socket.write(data);
+    };
 };
 
-AdapterTLS.prototype.send = function (data) {
-    this.socket.write(data);
+AdapterTLS.prototype.onOpen = function (callback) {
+    this.state.whenEqual(true, callback);
+};
+
+AdapterTLS.prototype.onEnd = function (callback) {
+    this.state.whenEqual(false, callback);
+};
+
+AdapterTLS.prototype.onError = function (callback) {
+    this.state.whenEqual(false, callback);
+};
+
+AdapterTLS.prototype.onData = function (ondata) {
+    this.ondata = ondata;
 };
 
 module.exports = AdapterTLS;
