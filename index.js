8947c4d6a4ff0bb564757e7c3c282e2d1b0c0d2f
vspirin <vspirin@spotware.com>
2017-03-13 14:46:16 +0200

    fix send method

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 'use strict';
 
-var tls = require('tls');
-var StateEmitter = require('state-emitter').StateEmitter;
+var
+    tls = require('tls'),
+    StateEmitter = require('state-emitter').StateEmitter;
 
 module.exports = function (params) {
     var
         ondata,
+        socket,
+        connected = true,
+        disconnected = false,
         host = params.host,
         port = params.port,
-        state = new StateEmitter(undefined);
+        state = new StateEmitter();
 
     return {
         connect: function () {
-            var
-                socket = tls.connect(port, host, function () {
-                    state.next(true);
-                });
-
+            socket = tls.connect(port, host, function () {
+                state.next(connected);
+            });
             socket.on('data', ondata);
             socket.on('end', function () {
-                state.next(false);
+                state.next(disconnected);
             });
             socket.on('error', function () {
-                state.next(false);
+                state.next(disconnected);
             });
-            this.send = function (data) {
-                socket.write(data);
-            };
+        },
+        send: function (data) {
+            socket.write(data);
         },
         onOpen: function (callback) {
-            state.whenEqual(true, callback);
+            state.whenEqual(connected, callback);
         },
         onEnd: function (callback) {
-            state.whenEqual(false, callback);
+            state.whenEqual(disconnected, callback);
         },
         onError: function (callback) {
-            state.whenEqual(false, callback);
+            state.whenEqual(disconnected, callback);
         },
         onData: function (callback) {
             ondata = callback;
